d15df7688783e6b1142870336f56189dbcadfbc5
Ahnaf Hasan Shifat <81911439+ah-naf@users.noreply.github.com>
2021-06-05 16:37:54 +0600

    Create 10003 - Cutting Sticks.cpp

---
+++ 10003 - Cutting Sticks.cpp
@@ -0,0 +1,79 @@
+/*________________________Bismillahir Rahmanir Rahim___________________
+                                                                      |
+|                    /\     |     | |\    |     /\     |-------        |
+|                   /  \    |     | | \   |    /  \    |               |
+|                  /----\   |-----| |  \  |   /----\   |-----          |
+|                 /      \  |     | |   \ |  /      \  |               |
+|                /        \ |     | |    \| /        \ |               |
+|                     14th batch CSE , ID : 12008029                   |
+|                     Comilla University                               |
+|                     Email : sheikhahnafshifat@gmail.com              |
+|______________________________________________________________________*/
+#include<bits/stdc++.h>
+#include<string>
+#include<iostream>
+using namespace std;
+#define ll                      long long
+#define MAX                     1000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+#define vii                     vector<int>
+#define vll                     vector<long long>
+#define vp                      vector<pair<ll, ll> >
+#define v_min(a)                *min_element(a.begin(),a.end())
+#define v_max(a)                *max_element(a.begin(),a.end())
+#define v_sum(a)                accumulate(a.begin(),a.end(),0)
+#define un(a)                   a.erase(unique(a.begin(),a.end()),a.end())
+#define SORT(a)                 sort(a.begin(),a.end())
+#define AHNAF                   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define YES                     cout<<"YES"<<endl
+#define NO                      cout<<"NO"<<endl
+#define testcase(t)             int t;cin>>t;while(t--)
+#define reverse(a)              reverse(a.begin(),a.end())
+#define ff                      first
+#define ss                      second
+#define print(a)                for(auto it: a) cout<<it<<" "
+#define all(a)                  a.begin(),a.end()
+#define sf(v)                   for(auto &it:v) cin>>it
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+int length;int n;int c[55], dp[55][55];
+
+int cost(int r, int l) {
+    return c[r]-c[l];
+}
+
+int func(int left, int right)
+{
+    if(dp[left][right] != -1) return dp[left][right];
+    if(left+1 == right) return dp[left][right] = 0;
+    int best = INT_MAX;
+    for(int i=left+1; i<right; i++) {
+        best = min(best, func(left,i)+func(i, right)+cost(right,left));
+    }
+    return dp[left][right] = best;
+}
+int main()
+{
+    //AHNAF;
+
+    while(scanf("%d", &length)) {
+        if(length == 0) break;
+         scanf("%d", &n);
+        for(int i=1; i<=n; i++) scanf("%d", &c[i]);
+        c[0] = 0, c[n+1] = length;
+        memset(dp, -1, sizeof(dp));
+        printf("The minimum cutting is %d.\n", func(0, n+1));
+    }
+
+}
+
+
